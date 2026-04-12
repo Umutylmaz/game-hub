914d5eeebc7ece2bbe8855caad8932b41685774d
umut.yilmaz@phimajor.com <umut.yilmaz@phimajor.com>
2026-04-12 23:18:08 +0300

    Displaying platform icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,15 +1,20 @@
-import { Card, Heading, Image } from '@chakra-ui/react';
+import { Card, Heading, Image, Text } from '@chakra-ui/react';
 import { Game } from '../hooks/useGames';
+import PlatformIconList from './PlatformIconList';
 
 interface Props {
   game: Game;
 }
+
 const GameCard = ({ game }: Props) => {
   return (
     <Card.Root borderRadius={10} overflow="hidden">
       <Image src={game.background_image}></Image>
       <Card.Body>
         <Heading fontSize="2xl">{game.name}</Heading>
+        <PlatformIconList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        ></PlatformIconList>
       </Card.Body>
     </Card.Root>
   );
