785497df8af89936097073502a62daac58d039ad
umut.yilmaz@phimajor.com <umut.yilmaz@phimajor.com>
2026-04-19 20:57:00 +0300

    Show a spinner while fetching genres

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card.Root width="100%" maxW="none">
+    <Card.Root>
       <Image src={getCroppedImageUrl(game.background_image)}></Image>
       <Card.Body>
         <Heading fontSize="2xl">{game.name}</Heading>
